8f2a13f013cebe53e511fb1e07dfc38a1f7fdcae
thomasR35 <thomasriou.35@gmail.com>
2024-11-15 15:55:55 +0100

    Last commit projet

--- frame/assets/js/annoucement-wall.js
+++ frame/assets/js/annoucement-wall.js
@@ -23,8 +23,10 @@ function announcementSubmit(event) {
 function createAnnouncementElement(data) {
   const newAnnouncement = document.createElement("div");
   newAnnouncement.classList.add("announcement");
-  newAnnouncement.innerHTML = `<h3>${data.title}</h3><p>${data.description}</p>`;
+  newAnnouncement.innerHTML = `<h5>${data.title}</h5><p>${data.description}</p>`;
   wall.appendChild(newAnnouncement);
+
+  rotatePostit(newAnnouncement);
 }
 
 function saveAnnouncement(data) {
@@ -43,6 +45,37 @@ function clearAllAnnouncements() {
   localStorage.removeItem("announcements");
 }
 
+function rotatePostit(postit) {
+  const randomRotation = Math.floor(Math.random() * 21) - 10;
+  postit.style.transform = `rotate(${randomRotation}deg)`;
+  postit.style.transition = "transform 0.5s ease";
+}
+
+// Fonction pour ajouter des post-its par défaut
+function addDefaultPostits() {
+  const defaultPostits = [
+    {
+      title: "Fruits à donner",
+      description:
+        "Mes pommiers ont bien donné cette année. J'ai beaucoup de surplus. Passez me voir, je suis dans le secteur de Bedée. Mon numéro : 06.32.15.14.45 — Sabine",
+    },
+    {
+      title: "Besoin d'un coup de main pour déménager",
+      description:
+        "Je déménage samedi prochain et j’aurais besoin de bras pour m’aider à porter quelques meubles. Pizza offerte ! Je suis à Pleumeleuc, contactez-moi au 07.21.65.43.22. Merci ! — Antoine",
+    },
+    {
+      title: "Service de jardinage gratuit",
+      description:
+        "Passionné de jardinage, je propose un coup de main gratuit pour les personnes ayant du mal à entretenir leur potager ou jardin. Que ce soit pour un désherbage, un coup de bêche, ou un conseil, je suis là ! Secteur : Montfort-sur-Meu. Contactez-moi au 06.45.23.87.10 — Jacques",
+    },
+  ];
+
+  defaultPostits.forEach((postit) => {
+    createAnnouncementElement(postit);
+  });
+}
+
 /**************************************************************/
 /*                         MAIN PROGRAMM                      */
 /**************************************************************/
@@ -52,7 +85,12 @@ window.addEventListener("DOMContentLoaded", function () {
 
   loadAnnouncements();
 
+  // load les postits par défaur au démarrage pourn la démo
+  addDefaultPostits();
+
   form.addEventListener("submit", announcementSubmit);
 
   clearButton.addEventListener("click", clearAllAnnouncements);
+
+  // rotatePostit();
 });
